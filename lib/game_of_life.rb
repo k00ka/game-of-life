7f00c7dce7e8919e76050ed5b0107a39e999a93e
jmks <jason.schweier@gmail.com>
2015-10-01 12:15:52 -0400

    simple cell that tracks its coordinates

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,6 +1,15 @@
 module GameOfLife
 
-  def self.xxx(yyy)
-  end
+  class Cell
+    attr_reader :x, :y
 
+    def initialize(x, y)
+      @x = x
+      @y = y
+    end
+
+    def coordinates
+      [x, y]
+    end
+  end
 end
--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,16 +1,29 @@
 #Advanced developers: Uncomment the following understand more about the RSpec "focus" filter
 #require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-require 'game_of_life'
+require "game_of_life"
 
 # The top of your testing block
 # More examples of tests can be found here: https://github.com/rspec/rspec-expectations
-RSpec.describe GameOfLife, "#something" do
+RSpec.describe GameOfLife::Cell do
 
-  it "returns an empty board given an empty board" do
-    expect(GameOfLife.step(empty_board)).to eq(empty_board)
+  context "when initialized" do
+    subject { described_class.new(1, 1) }
+
+    it { is_expected.to be_an_instance_of described_class }
+
+    it { is_expected.to respond_to :x }
+    it { is_expected.to respond_to :y }
   end
 
-  # more tests go here
-end
+  describe "#coordinates" do
+    subject { described_class.new(4, 3) }
 
+    it { is_expected.to respond_to :coordinates }
+
+    it "returns enumerable of coordinates" do
+      expect(subject.coordinates.first).to eql 4
+      expect(subject.coordinates.last).to  eql 3
+    end
+  end
+end
