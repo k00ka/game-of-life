01c10dc471c44c33e2e7150de30fd878ded4709d
jmks <jason.schweier@gmail.com>
2015-10-01 12:36:47 -0400

    a simple world for cells to live in

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -12,4 +12,19 @@ module GameOfLife
       [x, y]
     end
   end
+
+  class World
+
+    def initialize(options = {})
+      options = {
+        cells: Array.new
+      }.merge(options)
+
+      @cells = options[:cells]
+    end
+
+    def cells
+      @cells.dup
+    end
+  end
 end
--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -27,3 +27,61 @@ RSpec.describe GameOfLife::Cell do
     end
   end
 end
+
+RSpec.describe GameOfLife::World do
+  it { is_expected.to be_an_instance_of described_class }
+
+  context "when initialized" do
+    context "with no cells" do
+      subject { described_class.new }
+
+      it "is an empty world" do
+        expect(subject.cells).to_not be_any
+      end
+    end
+
+    context "with cells" do
+      subject { described_class.new cells: [random_cell] }
+
+      it "is a world with life!" do
+        expect(subject.cells).to be_any
+      end
+    end
+  end
+
+  describe "#cells" do
+    it { is_expected.to respond_to :cells }
+
+    context "with no cells" do
+      subject { described_class.new }
+
+      it "does not return any" do
+        expect(subject.cells).to_not be_any
+      end
+    end
+
+    context "with a cell" do
+      let(:cell) { random_cell }
+      subject { described_class.new(cells: [cell]) }
+
+      it "contains the cell" do
+        expect(subject.cells).to include cell
+      end
+    end
+
+    context "with many cells" do
+      let(:cells) { [random_cell, random_cell, random_cell] }
+      subject { described_class.new(cells: cells) }
+
+      it "contains the cells" do
+        expect(subject.cells).to include *cells
+      end
+    end
+  end
+
+  def random_cell(range = (-10..10))
+    points = range.to_a
+
+    GameOfLife::Cell.new(points.sample, points.sample)
+  end
+end
